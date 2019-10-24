81f134119c0403f89447475c055c58dd65cd17c0
nataliiia11 <37028853+nataliiia11@users.noreply.github.com>
2019-10-24 16:57:39 +0200

    Add files via upload

---
+++ Konto.java
@@ -0,0 +1,12 @@
+package Uebung2;
+
+public class Konto {
+private String blz;
+private String kontoNr;
+private String name;
+private String bank;
+private Aktion aktion;
+private Spende spende;
+//later we will create a method that take spende and forward it to aktion without saving it in konto class
+
+}
---
+++ Spende.java
@@ -0,0 +1,5 @@
+package Uebung2;
+
+public class Spende {
+
+}
